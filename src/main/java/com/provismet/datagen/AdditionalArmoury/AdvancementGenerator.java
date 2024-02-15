f2077f8f29f087b653bff92c30c887dbac9ffa2e
Provismet <17149901+Provismet@users.noreply.github.com>
2024-02-15 15:14:19 +0000

    Revert "Merge branch '1.20.2' into 1.20"
    This reverts commit 87e0b8ae91251f943b2c24fa42318cbd4ccb3226, reversing
    changes made to 03dd97a23bd0ffe38cd6f7562047a83899c21fba.

--- src/main/java/com/provismet/datagen/AdditionalArmoury/AdvancementGenerator.java
+++ src/main/java/com/provismet/datagen/AdditionalArmoury/AdvancementGenerator.java
@@ -1,6 +1,5 @@
 package com.provismet.datagen.AdditionalArmoury;
 
-import java.util.Optional;
 import java.util.function.Consumer;
 
 import com.provismet.AdditionalArmoury.AdditionalArmouryMain;
@@ -10,11 +9,8 @@ import com.provismet.AdditionalArmoury.registries.AAItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
 import net.minecraft.advancement.Advancement;
-import net.minecraft.advancement.AdvancementCriterion;
-import net.minecraft.advancement.AdvancementEntry;
 import net.minecraft.advancement.AdvancementFrame;
 import net.minecraft.advancement.AdvancementRewards;
-import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.advancement.criterion.EnchantedItemCriterion;
 import net.minecraft.advancement.criterion.ImpossibleCriterion;
 import net.minecraft.advancement.criterion.InventoryChangedCriterion;
@@ -23,6 +19,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionUtil;
 import net.minecraft.potion.Potions;
 import net.minecraft.predicate.NumberRange;
+import net.minecraft.predicate.entity.LootContextPredicate;
 import net.minecraft.predicate.item.ItemPredicate;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -33,16 +30,16 @@ public class AdvancementGenerator extends FabricAdvancementProvider {
     }
 
     @Override
-    public void generateAdvancement (Consumer<AdvancementEntry> consumer) {
+    public void generateAdvancement (Consumer<Advancement> consumer) {
         ItemStack enchantedStaff = AAItems.STAFF.getDefaultStack();
         enchantedStaff.addEnchantment(AAEnchantments.BOOST, 1);
 
         ItemStack bakuretsu = AAItems.STAFF.getDefaultStack();
         bakuretsu.addEnchantment(AAEnchantments.EXPLOSION, 1);
 
-        AdvancementCriterion<EnchantedItemCriterion.Conditions> staffCondition = Criteria.ENCHANTED_ITEM.create(new EnchantedItemCriterion.Conditions(Optional.empty(), Optional.of(ItemPredicate.Builder.create().items(AAItems.STAFF).build()), NumberRange.IntRange.ANY));
-        AdvancementEntry enchanter = Advancement.Builder.create().build(new Identifier("story/enchant_item"));
-        AdvancementEntry enchantStaff = Advancement.Builder.create().parent(enchanter)
+        EnchantedItemCriterion.Conditions staffCondition = new EnchantedItemCriterion.Conditions(LootContextPredicate.EMPTY, ItemPredicate.Builder.create().items(AAItems.STAFF).build(), NumberRange.IntRange.ANY);
+        Advancement enchanter = Advancement.Builder.create().build(new Identifier("story/enchant_item"));
+        Advancement enchantStaff = Advancement.Builder.create().parent(enchanter)
             .display(
                 enchantedStaff,
                 this.buildTranslationKey("enchant_staff.title"),
@@ -67,10 +64,10 @@ public class AdvancementGenerator extends FabricAdvancementProvider {
                 true,
                 false
             )
-            .criterion("used_explosion_magic", Criteria.IMPOSSIBLE.create(new ImpossibleCriterion.Conditions()))
+            .criterion("used_explosion_magic", new ImpossibleCriterion.Conditions())
             .build(consumer, AdditionalArmouryMain.identifier("story/explosion_magic").toString());
 
-        AdvancementEntry dragonBreath = Advancement.Builder.create().build(new Identifier("end/dragon_breath"));
+        Advancement dragonBreath = Advancement.Builder.create().build(new Identifier("end/dragon_breath"));
         Advancement.Builder.create().parent(dragonBreath)
             .display(
                 PotionUtil.setPotion(AAItems.DIAMOND_DAGGER.getDefaultStack(), Potions.POISON),
@@ -85,7 +82,7 @@ public class AdvancementGenerator extends FabricAdvancementProvider {
             .criterion("crafted_tipped_dagger", RecipeCraftedCriterion.Conditions.create(AdditionalArmouryMain.identifier("tipped_dagger")))
             .build(consumer, AdditionalArmouryMain.identifier("end/craft_tipped_dagger").toString());
 
-        AdvancementEntry ancientDebris = Advancement.Builder.create().build(new Identifier("nether/obtain_ancient_debris"));
+        Advancement ancientDebris = Advancement.Builder.create().build(new Identifier("nether/obtain_ancient_debris"));
         Advancement.Builder.create().parent(ancientDebris)
             .display(
                 AAItems.OVERNETHER_CHESTPLATE,
